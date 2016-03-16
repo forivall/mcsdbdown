818c882e499a3ccdbd84e787ca0e241a260e2ebc
Emily Marigold Klassen <forivall@gmail.com>
2016-03-16 16:56:17 -0700

    chore: setup base files

---
+++ index.js
@@ -0,0 +1,4 @@
+
+module.exports = function(app) {
+  // app is an express 3 Application
+}
---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+
+// TODO: mock MCS environment so that we can properly run unit tests
+
+import test from 'ava'
+
+test('it works', t => {
+  t.pass()
+})
