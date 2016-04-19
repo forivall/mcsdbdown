480a98d5a5422d265b7d3126819ed7c7def99293
Emily Marigold Klassen <forivall@gmail.com>
2016-04-19 12:58:51 -0700

    feat: add an example route

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
+const name = require('./package.json').name;
+const root = '/mobile/custom/' + name;
+
 module.exports = function (app) {
   // app is an express 3 Application
+  app.get(root + '/echo', function (req, res) {
+    res.send(200, req.body);
+  });
 };
